fa2b773a9757db52ec64f158c951969e49a8fcca
dletkiewicz <jokerek99@gmail.com>
2025-08-24 14:40:09 +0200

    init structure and auth implementation

---
+++ src/main/java/pl/levelup/account/application/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package pl.levelup.account.application.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+//todo
+}
